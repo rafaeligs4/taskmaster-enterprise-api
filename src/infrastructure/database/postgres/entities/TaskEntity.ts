f746829e3fe5a534b1dd0ddb325f71795e1360c3
Rigsby001 <ignaciorafaelgs4@gmail.com>
2026-01-09 12:58:16 -0400

    Migracion completa  de postgres

--- src/infrastructure/database/postgres/entities/TaskEntity.ts
+++ src/infrastructure/database/postgres/entities/TaskEntity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryColumn, Column } from "typeorm";
+import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { UserEntity } from "./UserEntity";
 
 @Entity({ name: "tasks" }) // Nombre de la tabla en SQL
 export class TaskEntity {
@@ -12,9 +13,14 @@ export class TaskEntity {
     @Column("text") // 'text' permite strings más largos que 'varchar'
     description: string;
 
-    @Column("boolean", { default: false })
-    completed: boolean;
+    @Column("int", { default: 0 })
+    statusTask: number;
 
     @Column("varchar")
     userId: string; // Por ahora lo dejamos como string simple (luego haremos la relación real)
+
+    // Relación ManyToOne: Muchas tareas pertenecen a un usuario
+    @ManyToOne(() => UserEntity, (user) => user.tasks)
+    @JoinColumn({ name: "userId" }) // Importante: Esto le dice a TypeORM que use la columna 'userId' de arriba para esta relación
+    user: UserEntity;
 }
--- src/infrastructure/database/postgres/entities/UserEntity.ts
+++ src/infrastructure/database/postgres/entities/UserEntity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { TaskEntity } from "./TaskEntity";
 
 
-@Entity()
+@Entity({ name: "users" })
 export class UserEntity {
     @PrimaryColumn()
     id: string;
@@ -14,4 +15,9 @@ export class UserEntity {
 
     @Column({ length: 256 })
     password: string;
+
+    // Un usuario tiene muchas tareas
+    @OneToMany(() => TaskEntity, (task) => task.user)
+    tasks: TaskEntity[];
+
 }
